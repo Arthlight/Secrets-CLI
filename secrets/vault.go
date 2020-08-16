27061a72480f9f3c0d35014c9af5dbe20ed94673
Arthlight <arthurpbh@gmail.com>
2020-08-16 20:27:24 +0200

    updated Get and set functions, added loadKeyValues and in general updated the code to support files

--- secrets/vault.go
+++ secrets/vault.go
@@ -2,41 +2,66 @@ package secrets
 
 import (
 	"Secrets-CLI/encrypt"
+	"encoding/json"
 	"errors"
+	"os"
 )
 
 type Vault struct {
 	encodingKey string
+	filePath string
 	keyValues map[string]string
 }
 
 
-func Memory(encodingKey string) Vault {
-	return Vault{
+func File(encodingKey, filePath string) *Vault {
+
+	return &Vault{
 		encodingKey: encodingKey,
-		keyValues: make(map[string]string),
+		filePath: filePath,
 	}
+}
+
+func (v * Vault) loadKeyValues() error {
+	file, err := os.Open(v.filePath)
+	if err != nil {
+		v.keyValues = make(map[string]string)
+		return nil
+	}
+	dec := json.NewDecoder(file)
+	err = dec.Decode(&v.keyValues)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (v *Vault) Get(key string) (string, error) {
-	hex, ok := v.keyValues[key]
-	if !ok {
-		return "", errors.New("secret: no value for that key")
-	}
-	ret, err := encrypt.Decrypt(v.encodingKey, hex)
+	err := v.loadKeyValues()
 	if err != nil {
 		return "", err
 	}
+	value, ok := v.keyValues[key]
+	if !ok {
+		return "", errors.New("secret: no value for that key")
+	}
 
-	return ret, nil
+	return value, nil
 }
 
 func (v *Vault) Set(key, value string) error {
+
 	encryptedValue, err := encrypt.Encrypt(v.encodingKey, value)
 	if err != nil {
 		return err
 	}
+	err = v.loadKeyValues()
+	if err != nil {
+		return err
+	}
 	v.keyValues[key] = encryptedValue
+	err = v.saveKeyValues()
 
-	return nil
+	return err
 }
